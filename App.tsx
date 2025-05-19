a9b105def7328250dbec2cd1ceec57359ade7b0d
mism-teknosure <m.ismail@teknosure.com>
2025-05-19 16:40:33 +0200

    feat: nativewind work on npm start

--- App.tsx
+++ App.tsx
@@ -1,24 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import "./global.css"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Bienvenue Medis</Text>
+    <View className="flex-1 items-center justify-center bg-gradient-to-b from-blue-50 via-blue-100 to-white">
+      <StatusBar style="auto" />
+      <View className="p-8 rounded-xl shadow-lg bg-white border-2 border-blue-200">
+        <Text className="text-3xl font-extrabold text-blue-700 text-center">
+          Bienvenue Medis
+        </Text>
+        <Text className="mt-2 text-blue-600 text-center font-medium">
+          Votre partenaire en solutions constructivesqsdfa
+        </Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#1E40AF',
-  },
-});
