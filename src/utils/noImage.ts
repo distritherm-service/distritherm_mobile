5b498d58de46d6767a614d4adcbd6ef1d376982c
mism-teknosure <m.ismail@teknosure.com>
2025-07-01 16:12:04 +0200

    feat: maj of product item

--- src/utils/noImage.ts
+++ src/utils/noImage.ts
@@ -1,1 +1,1 @@
-export const NO_IMAGE_URL = "https://img.freepik.com/vecteurs-premium/icone-interface-profil-art-vectoriel_1015832-3774.jpg?semt=ais_hybrid&w=740";
+export const NO_IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg";
